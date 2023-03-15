91162ef43e13dcb49ab3de3773cbbc431ea96ebb
stijnveeke <st.van.eekelen@gmail.com>
2023-03-15 14:45:56 +0100

    Testing envs

--- src/envcheck.php
+++ src/envcheck.php
@@ -2,8 +2,20 @@
 
 namespace Stijn\YggiPackageTest;
 
-if (!isset($_ENV['DB_HOST'])) {
-    throw new \Exception('Missing ENV value DB_HOST');
+$env_values = [
+    'SNAPPY_DB_HOST',
+    'SNAPPY_DB_NAME',
+    'SNAPPY_DB_USER',
+    'SNAPPY_DB_PASS'
+];
+
+foreach ($env_values as $env) {
+    if (!isset($_ENV[$env])) {
+        throw new \Exception("Missing ENV value ${env}");
+    }
 }
 
-define("Stijn\YggiPackageTest\DB_HOST", $_ENV['DB_HOST']);
+define("Stijn\YggiPackageTest\DB_HOST", $_ENV['SNAPPY_DB_HOST']);
+define("Stijn\YggiPackageTest\DB_NAME", $_ENV['SNAPPY_DB_NAME']);
+define("Stijn\YggiPackageTest\DB_USER", $_ENV['SNAPPY_DB_USER']);
+define("Stijn\YggiPackageTest\DB_PASS", $_ENV['SNAPPY_DB_PASS']);
